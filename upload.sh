fbe10fdfc2e1a0bc8f18c0e19060cd1d2185edf4
thebetioplane <thebetioplane@users.noreply.github.com>
2025-07-30 22:56:00 -0400

    Makes small change to the upload script to take username as a parameter.

--- upload.sh
+++ upload.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
+
 WHICH_DIR=~/Desktop/editor_upload
 rm -rf $WHICH_DIR
-test "$1" = clean && exit 0
+
+if [ -z "$2" ]; then
+	echo 'Usage:' $0 ' {github_username} {ssh_config}'
+	echo ' - OR -'
+	echo 'Usage:' $0 ' clean'
+	exit 1
+fi
+
 cp -R distro $WHICH_DIR
 cp -r distro_aux/. $WHICH_DIR
 cd $WHICH_DIR
 git init
-# Change to your own github information / ssh key
-# #######
-git config user.name thebetioplane
-git config user.email thebetioplane@users.noreply.github.com
-git remote add origin github-thebetioplane:thebetioplane/osuReplayEditorV3.git
-# #######
+git config user.name $1
+git config user.email $1'@users.noreply.github.com'
+git remote add origin $2':'$1'/osuReplayEditorV3.git'
 git add --all
 git commit -m "`date '+%Y-%m-%d [%y%j-%H]'`"
 git tag beta
